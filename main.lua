6195241f71ddacb01a09527636a43db597f6c624
Jos3k4 <jos3k4@gmail.com>
2014-09-08 21:37:03 +0200

    Dos personajes en juego

--- main.lua
+++ main.lua
@@ -1,44 +1,54 @@
-function generaHeroe()
+function generaHeroe(h,posx,posy,spd,lf,atck,def)
  -- Creamos una tabla como estructura
- hero.x = 300    -- x,y establecemos las coordenadas
- hero.y = 450
- hero.speed = 100
- hero.life = 20
- hero.attack = 4
- hero.defense = 2
-return hero
+ h = {}
+ h.x = posx    -- x,y establecemos las coordenadas
+ h.y = posy
+ h.speed = spd
+ h.life = lf
+ h.attack = atck
+ h.defense = def
+return h
 end
 
 function forjarArma(a,b)
 arma.ataque = a
 arma.tipo = b
-
 return arma
 end
 
 -- Inicializacion del Juego.
 function love.load()
--- Creo mi tabla heroe vacio.
-hero = {}
+personajes = {}
+
+heroe = {}
+antiheroe = {}
 --De esta forma voy a crear un heroe generico.
 -- En un futuro lo ideal seria pasarle los parametros para asi generar en un momento dado el heroe que quiera.
-hero = generaHeroe()
+hero = generaHeroe(hero,450,300,100,20,4,2)
+antihero = generaHeroe(antiheroe,200,200,100,10,1,6)
+
+personajes[0] = hero
+personajes[1] = antihero
+
 --Arma del heroe.
 arma = {}
 arma = forjarArma(100,"Espada de fuego")
+
 -- Equipamos el arma al heroe.
 hero.equipada = arma
+
 -- Inicialmente el arma esta desenfundada.
-desenfundada = false
+hero.desenfundada = false
+
 --Vamos a cargar los temas que pueden sonar en el juego.
 temazo = love.audio.newSource("resources/music/Deeper.ogg")
 combate = love.audio.newSource("resources/music/AssaultOnMistCastle.ogg")
 --Reproducimos el tema base.
-love.audio.play(temazo)
+--love.audio.play(temazo)
 
-valor = ""
 --Descompongo el Spritesheet en fragmentos.
 tilesetImage = love.graphics.newImage("resources/images/human_base.png")
+
 
   local tileW, tileH = 16,20
   local tilesetW, tilesetH = tilesetImage:getWidth(), tilesetImage:getHeight()
@@ -54,9 +64,8 @@ tilesetImage = love.graphics.newImage("resources/images/human_base.png")
   AnimacionAbajo[1] = love.graphics.newQuad(16,  36, tileW, tileH, tilesetW, tilesetH)
   AnimacionAbajo[2] = love.graphics.newQuad(32,  36, tileW, tileH, tilesetW, tilesetH)
   
-  Animacion = AnimacionArriba[0]
-  
-  anim = newAnimation(tilesetImage, 16, 20, 0.1, 0)
+  hero.animacion = AnimacionArriba[0]
+  antihero.animacion = AnimacionAbajo[0]
  
 end
 
@@ -64,21 +73,21 @@ function animacionArriba()
   if i == 2 then
       i = 0
   end  
-Animacion = AnimacionArriba[i]
+hero.animacion = AnimacionArriba[i]
   i = i + 1
 end 
 
 
 function love.update(dt)
   if love.keyboard.isDown("left") then
-   hero.x = hero.x - hero.speed*dt
+   personajes[0].x = personajes[0].x - personajes[0].speed*dt
  elseif love.keyboard.isDown("right") then
-   hero.x = hero.x + hero.speed*dt
+   personajes[0].x = personajes[0].x + personajes[0].speed*dt
  elseif love.keyboard.isDown("down") then
-   hero.y = hero.y + hero.speed*dt
-   animacionAbajo()
+   personajes[0].y = personajes[0].y + personajes[0].speed*dt
+   --animacionAbajo()
  elseif love.keyboard.isDown("up") then
-   hero.y = hero.y - hero.speed*dt
+   personajes[0].y = personajes[0].y - personajes[0].speed*dt
    animacionArriba()
  end
  
@@ -87,7 +96,7 @@ function love.update(dt)
    love.audio.stop(temazo)
    love.audio.play(combate)
    love.graphics.setColor(255,0,0,255)
-   desenfundada = true
+   hero.desenfundada = true
  end
  
 end
@@ -98,13 +107,21 @@ function love.draw()
  love.graphics.rectangle("fill", 0, 465, 800, 150)
 
  
- if desenfundada then
+if hero.desenfundada then
  love.graphics.print(hero.equipada.tipo, 100, 100)
 end
 --Transparencia al color de la escena.
 love.graphics.setColor(255, 255, 255, 255); 
-love.graphics.draw(tilesetImage,Animacion,hero.x,hero.y)
 
- love.graphics.print(valor, 200, 200)
+--print(personajes[0].x)
+--for i=0,1,1 do
+--  love.graphics.draw(tilesetImage,hero.animacion,personajes[i].x,personajes[i].y)
+--end
+
+
+love.graphics.draw(tilesetImage,personajes[0].animacion,personajes[0].x,personajes[0].y)
+love.graphics.draw(tilesetImage,personajes[1].animacion,personajes[1].x,personajes[1].y)
+love.graphics.print(personajes[0].x, 100, 100)
+love.graphics.print(personajes[1].x, 100, 130)
 
 end
